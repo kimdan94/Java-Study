a2885a4c0a295300c0035d987b570a7979657771
kimdan94 <dana1939@naver.com>
2024-03-18 17:33:35 +0900

    Operator2

---
+++ src/Operator/Operator4.java
@@ -0,0 +1,9 @@
+package Operator;
+public class Operator4 {
+    public static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3; //(2 * 2) + (3 * 3)
+        int sum4 = (2 * 2) + (3 * 3); //동일함
+        System.out.println("sum3 = " + sum3);
+        System.out.println("sum4 = " + sum4);
+    }
+}
